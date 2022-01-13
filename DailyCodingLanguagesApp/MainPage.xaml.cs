12cba261c5d88504470945dcfcffa29550e7f07c
Silas Maughan <silas.maughan@gmail.com>
2022-01-13 23:07:13 +0000

    Init

---
+++ DailyCodingLanguagesApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DailyCodingLanguagesApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (Language.Text != "Python")
+            {
+                Language.Text = "Python";
+            }
+            else
+            {
+                Language.Text = "Lisp";
+            }
+        }
+    }
+
+   
+}
